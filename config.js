44a56a4a2756779ebb59182544c98ddef8e21798
thangbd4 <81812523+bt1401@users.noreply.github.com>
2025-05-20 16:18:45 +0700

    init project

---
+++ config.js
@@ -0,0 +1,20 @@
+const CONFIG = {
+    titleWeb: "Gửi Tu Tuy",
+    introTitle: 'Bé Iu à',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: '^^HiHi^^',
+    title: 'Bé ơi a biết a sai gòi, E tha lỗi cho a nhoaa 🥰',
+    desc: 'Thoát web chấp nhận tha lỗi cho anh rùi nhé!!!',
+    btnYes: 'E tha lỗi mà, Iu a námmmm ❤️',
+    btnNo: 'Không nha :3',
+    question: 'Yahhh em đồng ý tha lỗi cho anh đúng hongg',
+    btnReply: 'Gửi cho anh ❤️',
+    reply: 'Em hết giận gòi, Em iu Anh námmmm 😘',
+    mess: 'Anh biết mà 🥰. Yêu bé nhìu nhìu 😘😘',
+    messDesc: 'Tối nay, anh qua đón bé đi chơi nha.',
+    btnAccept: 'Okiiiii lun ❤️',
+    messLink: 'https://www.instagram.com/direct/t/101611331242296/'
+}
